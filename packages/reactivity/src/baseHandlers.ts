b38528eab5fa926e51c5ba5110bff8e4c5ffe25d
andbreak <1622170503@qq.com>
2024-05-28 18:33:20 +0800

    处理代理对象

---
+++ packages/reactivity/src/baseHandlers.ts
@@ -0,0 +1,4 @@
+/**
+ * 响应性的 handler （Proxy 的处理对象）
+ */
+export const mutableHandlers: ProxyHandler<object> = {}
---
+++ packages/reactivity/src/reactive.ts
@@ -0,0 +1,41 @@
+import { mutableHandlers } from './baseHandlers'
+
+/**
+ * 响应式 weakMap 缓存对象
+ */
+export const reactiveMap = new WeakMap()
+
+/**
+ *
+ * @param target 需要响应式的对象
+ * @param baseHandlers 响应式的处理对象配置
+ * @param proxyMap 响应式 weakMap 缓存对象
+ */
+export const createReactiveObject = (
+  target: object,
+  baseHandlers: ProxyHandler<any>,
+  proxyMap: WeakMap<object, any>
+) => {
+  // 优化机制，如果被响应式的对象已经是一个响应式了，就不需要再进行观测，直接返回
+  const existingProxy = proxyMap.get(target)
+  if (existingProxy) {
+    return existingProxy
+  }
+
+  // 如果不是，则需要根据 target 创建一个代理对象
+  const proxy = new Proxy(target, baseHandlers)
+
+  // 缓存代理对象
+  reactiveMap.set(target, proxy)
+
+  return proxy
+}
+
+/**
+ * 响应式Api - reactive
+ * @param target: 需要响应式的对象
+ * @returns Proxy 实例
+ */
+export const reactive = (target: object) => {
+  return createReactiveObject(target, mutableHandlers, reactiveMap)
+}
